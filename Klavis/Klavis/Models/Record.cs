24c2fd728c77eeb10915e43e05b5712662c903f5
isaacfc4 <60206415+isaacfc4@users.noreply.github.com>
2022-02-03 15:11:16 -0500

    Fixed some stuff
    It now has the functions it needs to access the history, but it is not working yet

---
+++ Klavis/Klavis/Models/Record.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Google.Cloud.Firestore;
+
+namespace Klavis.Models
+{
+    [FirestoreData]
+    public class Record
+    {
+        public string userID { get; set; }
+        public DateTime timestamp { get; set; }
+        [FirestoreProperty]
+        public string firstName { get; set; }
+        [FirestoreProperty]
+        public string lastName { get; set; }
+        [FirestoreProperty]
+        public string pictureLink { get; set; }
+        [FirestoreProperty]
+        public bool accessStatus { get; set; }
+        [FirestoreProperty]
+        public bool accountStatus { get; set; }
+
+    }
+}
