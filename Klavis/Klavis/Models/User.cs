a801c953811e1f492b118ff1e10b9d6a9cb0ff5b
isaacfc4 <60206415+isaacfc4@users.noreply.github.com>
2022-02-28 20:29:21 -0500

    Many changes
    I removed the history page and integrated it into the checkin page. I fixed the history queries so that they can be sorted and displayed in different quantities. I tied up a bunch of edge cases. I added a location and # of entries dropdown to thhe history section. I changed the input to take the full output of the vtap, including the card serial. I added functions to parse the new input scheme. I cleaned up how user data is maintained in the program. I adjusted the add user and edit user pages to a new format to account for a change in the fields of the database. I modified some of elizabeths changes to the user management page to have the card icon display in red instead of the entire row. I added an add user button to the user management page.

--- Klavis/Klavis/Models/User.cs
+++ Klavis/Klavis/Models/User.cs
@@ -8,17 +8,24 @@ namespace Klavis.Models
     {
         public string userID { get; set; }
         [FirestoreProperty]
+        public bool accountStatus { get; set; }
+        [FirestoreProperty]
+        public bool cardCreated { get; set; }
+        [FirestoreProperty]
+        public string cardSerial { get; set; }
+        [FirestoreProperty]
+        public DateTime dateCreated { get; set; }
+        [FirestoreProperty]
+        public string email { get; set; }
+        [FirestoreProperty]
         public string firstName { get; set; }
         [FirestoreProperty]
         public string lastName { get; set; }
         [FirestoreProperty]
         public string pictureLink { get; set; }
-        [FirestoreProperty]
-        public bool accountStatus { get; set; }
-        [FirestoreProperty]
-        public bool cardCreated { get; set; }
-        [FirestoreProperty]
-        public DateTime dateCreated { get; set; }
+        
+        
+        
 
     }
     
