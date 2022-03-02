a25b66061bb6c54c52b6b28ed993efe56ce7ddfc
Elizabeth Diamond <diamond7@bu.edu>
2022-03-01 23:25:01 -0500

    MAIN CHANGE ADD ADMIN.CS
    Create an admin class using the admin collection from the Firebase database to be used to verify admin login to the site

---
+++ Klavis/Klavis/Models/Admin.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Google.Cloud.Firestore;
+
+namespace Klavis.Models
+{
+	[FirestoreData]
+	public class Admin
+	{
+		[FirestoreProperty]
+		public string email { get; set; }
+	}
+}
