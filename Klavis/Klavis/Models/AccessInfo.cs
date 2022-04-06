10078488b12b43b99c0ffe4b0e4fc5b199886f41
isaacfc4 <60206415+isaacfc4@users.noreply.github.com>
2022-04-06 13:45:19 -0400

    Updated UI
    Added UI components from Amir, but did not use the entire UI commit.

---
+++ Klavis/Klavis/Models/AccessInfo.cs
@@ -0,0 +1,15 @@
+﻿namespace Klavis.Models
+{
+    public class AccessInfo
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string Terminal { get; set; }
+        public string Id { get; set; }
+
+        public string TimeStamp { get; set; }
+
+        public bool Determination { get; set; }
+    }
+}
